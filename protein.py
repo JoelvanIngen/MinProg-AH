cb8b89e4bac357f69d460a5e50570e7a81928d3d
Joël van Ingen <j.c.ingen@gmail.com>
2024-01-08 18:48:07 +0100

    Fix type collision for Protein.order between list and numpy.ndarray

--- protein.py
+++ protein.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from numpy.typing import NDArray
+
 
 class Protein:
 
-	def __init__(self, sequence: str, order: list = None) -> None:
+	def __init__(self, sequence: str, order: NDArray = None) -> None:
 		# sequence as ints: 1 = H, 2 = P
 		self.sequence = sequence
 		#self.seq_as_int = [1 if acid == 'H' else 2 for acid in sequence]
 		# initial order: straight line
-		if order:
+		if order is not None:
 			self.order = order
 		else:
 			self.order = np.ones(len(sequence) - 1)
@@ -20,8 +22,8 @@ class Protein:
 		plt.ylim(-len(self.sequence), len(self.sequence))
 		x, y = 0, 0
 		for i, acid in enumerate(self.sequence):
-			x_new = self.order[:i].count(1) - self.order[:i].count(-1)
-			y_new = self.order[:i].count(2) - self.order[:i].count(-2)
+			x_new = np.count_nonzero(self.order[:i] == 1) - np.count_nonzero(self.order[:i] == -1)
+			y_new = np.count_nonzero(self.order[:i] == 2) - np.count_nonzero(self.order[:i] == -2)
 			plt.text(x_new, y_new, acid, size='10')
 			plt.plot([x, x_new], [y, y_new], '--', color='black', linewidth=1)
 			x, y = x_new, y_new
@@ -30,6 +32,6 @@ class Protein:
 
 if __name__ == "__main__":
 	seq = "HHPHHHPH"
-	order = [1,2,1,-2,-2,-1,-2]
+	order = np.array([1,2,1,-2,-2,-1,-2])
 	protein = Protein(seq, order)
 	protein.plot()
