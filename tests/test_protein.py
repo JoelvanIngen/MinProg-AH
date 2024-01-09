66fa6b43bfc4677295b520b53f6b68f17c61a687
Jarecs <Jarecschouten@gmail.com>
2024-01-09 11:44:40 +0100

    Added test for new Protein/Node functionality

--- tests/test_protein.py
+++ tests/test_protein.py
@@ -2,6 +2,7 @@ import pytest
 
 import numpy as np
 from protein_folding.protein import Protein, InvalidSequenceError
+from protein_folding.definitions import *
 
 
 def test_create_valid_protein():
@@ -13,3 +14,11 @@ def test_create_invalid_protein():
     with pytest.raises(InvalidSequenceError):
         seq = "HHPHHAPH"
         _ = Protein(seq)
+
+
+def test_protein():
+    seq = "HHHH"
+    protein = Protein(seq)
+    order = [LEFT, LEFT, LEFT]
+    protein.set_order(order)
+    assert protein.get_order_quality() == 3
