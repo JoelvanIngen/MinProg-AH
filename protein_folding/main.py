0ca8c2f26cff0c9e89db1a56def80d174e2dc3d9
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-10 10:48:46 +0100

    Add main.py for user interaction

---
+++ protein_folding/main.py
@@ -0,0 +1,31 @@
+import sys
+from .protein import Protein
+
+
+def get_sequence() -> str:
+    match len(sys.argv):
+        case 1:
+            sequence = input("Enter sequence: ")
+        case 2:
+            sequence = sys.argv[2]
+        case _:
+            print("Usage: python main.py [sequence] or python main.py and input sequence manually.")
+            sys.exit(1)
+
+    return sequence
+
+
+def main():
+    sequence = get_sequence()
+    protein = Protein(sequence)
+
+    # Algorithm stuff??
+    # algorithm = Algorithm(protein)
+    # algorithm.optimise() / algorithm.run() or any cool name we come up with
+
+    # Visualisation??
+    protein.plot()
+
+
+if __name__ == '__main__':
+    main()
