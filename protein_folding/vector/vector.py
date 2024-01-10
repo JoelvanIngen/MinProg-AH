e41ed69879b0a43e9a9bf2c5dddb7cfe6e522bd1
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-10 09:37:04 +0100

    Add area and volume methods to Vec3D

--- protein_folding/vector/vector.py
+++ protein_folding/vector/vector.py
@@ -43,6 +43,9 @@ class Vec3D:
     def abs_diff(cls, vec1: 'Vec3D', vec2: 'Vec3D') -> 'Vec3D':
         return abs(vec1 - vec2)
 
+    def area(self) -> int:
+        return self.x * self.y
+
     def eq_components(self, eq_val: int) -> tuple[bool, bool, bool]:
         return self.x == eq_val, self.y == eq_val, self.z == eq_val
 
@@ -51,3 +54,6 @@ class Vec3D:
 
     def len_sq(self) -> int:
         return self.x ** 2 + self.y ** 2 + self.z ** 2
+
+    def volume(self) -> int:
+        return self.x * self.y * self.z
