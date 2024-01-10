0835edc005dcea09d3630e729a5fa9fa0f8d62a4
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-10 11:11:02 +0100

    Remove unused code in vector.tools.py

--- protein_folding/vector/tools.py
+++ protein_folding/vector/tools.py
@@ -12,9 +12,5 @@ def get_min_max(vectors: list[Vec3D]) -> tuple[Vec3D, Vec3D]:
     y = [v.y for v in vectors]
     z = [v.z for v in vectors]
 
-    dx = max(x) - min(x)
-    dy = max(y) - min(y)
-    dz = max(z) - min(z)
-
     return (Vec3D(min(x), min(y), min(z)),
             Vec3D(max(x), max(y), max(z)))
