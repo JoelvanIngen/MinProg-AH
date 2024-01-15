ad215a73fec994bbf00d8662cf8368241cae025f
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-15 11:23:21 +0100

    Add len(30) experiments
    Co-authored-by: Jarecs <61419537+Jarecs@users.noreply.github.com>

---
+++ experiments/evaluate_purerandom_protein_len30.py
@@ -0,0 +1,21 @@
+from experiments_helper import create_experiment_folders
+from protein_folding.protein import Protein
+from protein_folding.algorithms import PureRandom
+
+
+def main():
+    create_experiment_folders()
+
+    sequence = 'H' * 30
+    protein = Protein(sequence)
+
+    algorithm = PureRandom(protein, dimensions=2, debug=True)
+    score = algorithm.run()
+
+    protein.plot('./output/evaluate_purerandom_protein_len30.png')
+
+    print(f'Final score: {score}')
+
+
+if __name__ == '__main__':
+    main()
