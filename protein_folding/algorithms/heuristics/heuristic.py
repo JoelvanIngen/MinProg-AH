d95de3562960f36c07022ac650e2fb60fc03953b
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-15 12:02:26 +0100

    Expand base heuristic and add docstrings
    Co-authored-by: Jarecs <61419537+Jarecs@users.noreply.github.com>

--- protein_folding/algorithms/heuristics/heuristic.py
+++ protein_folding/algorithms/heuristics/heuristic.py
@@ -1,6 +1,32 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from protein_folding.protein import Protein
+    from protein_folding.node import Node
+
+
 class Heuristic:
-    def __init__(self, weight: float = 1.):
+    def __init__(self, protein: Protein, weight: float = 1.):
+        """
+        Initialises the heuristic
+
+        pre:
+            - protein is a protein object to run on
+            - weight is a float indicating how the heuristic should weigh its
+            final output
+        """
+        self.protein = protein
         self.weight = weight
 
-    def run(self, direction: list[int]) -> list[float]:
+    def run(self, node: Node, directions: list[int]) -> list[float]:
+        """
+        Runs the heuristic and computes a score for each possible option
+
+        pre:
+            - node is a Node object to apply the heuristic on
+            - directions is a list of directions that a node can take from
+            its source (the previous node).
+        post:
+            - a float is returned representing a score for every direction.
+        """
         pass
