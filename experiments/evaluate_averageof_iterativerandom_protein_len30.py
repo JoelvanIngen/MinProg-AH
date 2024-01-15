92a0c18bfe22e9dcea605f3ed0f93d3688d99b99
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-15 13:46:14 +0100

    Add hist plots to experiments

--- experiments/evaluate_averageof_iterativerandom_protein_len30.py
+++ experiments/evaluate_averageof_iterativerandom_protein_len30.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from experiments_helper import create_experiment_folders
 from protein_folding.protein import Protein
 from protein_folding.algorithms import IterativeRandom
@@ -22,6 +24,18 @@ def main():
 
     print(f'Average final score: {score_avg}')
     print(f"All scores: {scores}")
+
+    plt.hist(scores, bins=10, color='blue', edgecolor='black')
+    plt.axvline(np.mean(scores), color='red', linestyle='dashed', linewidth=2, label=f'Average: {np.mean(scores):.2f}')
+    plt.axvline(min(scores), color='green', linestyle='dashed', linewidth=2, label=f'Min: {min(scores)}')
+    plt.axvline(max(scores), color='orange', linestyle='dashed', linewidth=2, label=f'Max: {max(scores)}')
+
+    # Adding labels and title
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Values with Average, Min, and Max')
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
