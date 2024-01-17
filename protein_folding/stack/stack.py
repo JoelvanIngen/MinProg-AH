585b2f3da245539749f0acb807b67e2c517b0b75
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-17 10:54:38 +0100

    Implement history stack

--- protein_folding/stack/stack.py
+++ protein_folding/stack/stack.py
@@ -1,8 +1,17 @@
 class Stack:
     def __init__(self):
-        self._order_history: list[list[int]] = []
+        self._direction_history: list[list[int]] = []
         self._ghost_history: list[list[bool]] = []
+        self._position_dict_history: list[dict] = []
 
-    def push(self, order: list[int], ghost: list[bool]):
-        self._order_history.append(order)
+    def push(self, order: list[int], ghost: list[bool], pos_dict: dict) -> None:
+        self._direction_history.append(order)
         self._ghost_history.append(ghost)
+        self._position_dict_history.append(pos_dict)
+
+    def pull(self) -> tuple[list[int], list[bool], dict]:
+        return (
+            self._direction_history.pop(),
+            self._ghost_history.pop(),
+            self._position_dict_history.pop(),
+        )
