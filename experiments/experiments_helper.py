8964e29ca5480815705b488cdaa5a495c857f6b9
Jarecs <Jarecschouten@gmail.com>
2024-01-17 19:55:32 +0100

    Added create_bruteforce_folders

--- experiments/experiments_helper.py
+++ experiments/experiments_helper.py
@@ -8,3 +8,8 @@ def create_experiment_folders():
     # Ensures folders are created so experiments don't crash
     if not os.path.exists('./output/'):
         os.makedirs('./output/')
+
+def create_bruteforce_folders():
+    # Ensures Brute Force folders are created so experiments don't crash
+    if not os.path.exists('./bf_output/'):
+        os.makedirs('./bf_output/')
