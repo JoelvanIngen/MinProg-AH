0fed1cac74e386766dd43c3b3cc6afd3c64b1726
Jarecs <Jarecschouten@gmail.com>
2024-01-18 10:54:08 +0100

    Rewrote BruteForce Evaluation
    Co-authored-by: JoelvanIngen <47477202+JoelvanIngen@users.noreply.github.com>

--- experiments/evaluate_bruteforce_protein.py
+++ experiments/evaluate_bruteforce_protein.py
@@ -3,84 +3,29 @@ import pstats
 
 from experiments_helper import create_bruteforce_folders
 from protein_folding.protein import Protein
-from random import shuffle
-import itertools
+from protein_folding.algorithms import BruteForce
 
-MAX_ITERATIONS = 1000000
-
-class ScoreTracker:
-    def __init__(self):
-        self.scores = {}
-        self.max_scores = 10
-        self.lowest_top_score = 0
-
-    def add_score(self, order, score):
-        # If the number of scores is already 10, remove the lowest score before adding a new one
-        if len(self.scores) == self.max_scores:
-            self.remove_worst_score()
-        self.scores[order] = score
-
-    def remove_worst_score(self):
-        if self.scores:
-            lowest_order = max(self.scores, key=self.scores.get)
-            del self.scores[lowest_order]
-
-    def get_worst_score(self):
-        if self.scores:
-            return self.scores[max(self.scores, key=self.scores.get)]
-        return None
-
-    def get_best_score(self):
-        if self.scores:
-            return self.scores[min(self.scores, key=self.scores.get)]
-        return None
-
-    def get_scores(self):
-        return self.scores
-
-
-def generate_combinations(n):
-    directions = [1, -1, 2, -2]
-    return list(list(itertools.product(directions, repeat=n)))
-
+MAX_ITERATIONS = 100000
 
 def run():
     # Create necessary folders
     create_bruteforce_folders()
 
-    seq = "HHPHHHPHPHHHPH"
+    seq = "HHPHHHPHHHHH"
+    # seq = "HHHHH"
+    dim = 2
+    protein = Protein(seq)
+    algorithm = BruteForce(protein, dimensions=dim, debug=True)
 
-    n = len(seq) - 1
-    order_list = generate_combinations(n)
-    configs = min(len(order_list), MAX_ITERATIONS)
-    shuffle(order_list)
-    print(f"Amount of configs: {configs}")
-
-    score_tracker = ScoreTracker()
-
-    for i, order in enumerate(order_list):
-        if i > MAX_ITERATIONS:
-            break
-        print(f"Checking config {i}/{configs} ({i / configs * 100:.0f}%)")
-        protein = Protein(seq)
-        protein.set_order(order)
-        if protein.has_valid_order():
-            score = protein.get_bond_score()
-            print(order, score)
-            if score <= score_tracker.lowest_top_score:
-                score_tracker.add_score(order, score)
-                score_tracker.lowest_top_score = score_tracker.get_worst_score()
-
-    results = score_tracker.get_scores()
+    results = algorithm.run()
     print(results)
-    print(len(results))
-    print(f"Best score: {score_tracker.get_best_score()}")
+    print(f"Lowest score: {min(results.values())}")
 
     for i, order in enumerate(results.keys()):
         protein = Protein(seq)
         protein.set_order(order)
         score = results[order]
-        protein.plot(filename=f'./bf_output/bf_#{i + 1}_score{score}.png')
+        # protein.plot(filename=f'./bf_output/bf_#{i + 1}_score{score}.png')
 
 
 if __name__ == '__main__':
@@ -88,3 +33,6 @@ if __name__ == '__main__':
     profiler.runcall(run)
     stats = pstats.Stats(profiler)
     stats.strip_dirs().sort_stats('cumulative').print_stats(10)
+    profiler.dump_stats('output/protein.prof')
+
+
