17dc2d3abf6615b7848db8c0ac4d5f5544eaf5e4
Jarecs <Jarecschouten@gmail.com>
2024-01-18 10:55:15 +0100

    Added logic to plot dictionaries of configurations
    Co-authored-by: JoelvanIngen <47477202+JoelvanIngen@users.noreply.github.com>

--- experiments/plot_predetermined_protein.py
+++ experiments/plot_predetermined_protein.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
     # Create necessary folders
     create_experiment_folders()
 
-    seq = "PHPPHPHH"
-    order = [UP, RIGHT, UP, LEFT, LEFT, DOWN, DOWN]
+    seq = "HHPHHHPHPHHHPH"
+    # order = [UP, RIGHT, UP, LEFT, LEFT, DOWN, DOWN]
+    orders = {(-1, -1, 2, 1, 2, 1, -2, 1, -2, -2, -2, -1, 2): -6, (2, 2, 1, -2, 1, -2, -1, -2, -1, -1, -1, 2, 1): -6, (2, -1, -2, -2, 1, 1, -2, -2, -1, 2, -1, -1, 2): -6, (-2, -1, 2, 2, 1, 2, -1, 2, -1, -2, -1, -2, 1): -6, (-1, 2, 1, 1, -2, 1, 2, 2, -1, -1, 2, -1, -2): -6, (-1, -1, -2, 1, 1, 1, 2, 1, 2, -1, -1, 2, 1): -6, (-1, -2, 1, 1, -2, 1, 2, 1, 2, -1, -1, 2, -1): -6, (-2, -2, -1, 2, 2, 2, 1, 1, -2, -2, 1, -2, -1): -6, (2, -1, -2, -1, 2, -1, -2, -2, 1, 1, 1, -2, -1): -6, (-2, 1, 2, 2, 1, 2, -1, 2, -1, -2, -1, -2, 1): -6}
+    print(min(orders.values()))
+    for i, order in enumerate(orders.keys()):
+        protein = Protein(seq)
+        protein.set_order(list(order))
 
-    protein = Protein(seq)
-    protein.set_order(order)
-
-    protein.plot(filename='./output/plot_predetermined_protein.png')
+        protein.plot(filename=f'./output/plot_predetermined_protein{i}.png')
