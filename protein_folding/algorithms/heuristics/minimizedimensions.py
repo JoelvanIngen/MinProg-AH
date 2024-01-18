5b18e5a0ab538c969621c5a2b9f1d14e1842d990
saif <saif-aknouch@hotmail.com>
2024-01-18 18:00:05 +0100

    finish the script

--- protein_folding/algorithms/heuristics/minimizedimensions.py
+++ protein_folding/algorithms/heuristics/minimizedimensions.py
@@ -7,11 +7,27 @@ class MinimizeDimensions(Heuristic):
     def __init__(self, *args):
         super().__init__(*args)
 
-    def _calc_size_score(self):
+    def _calc_size_score(self, left_upper, right_lower):
         """
         Computes a score based on the physical dimensions of the protein. This
             value can be used as a penalty for a specific configuration.
         """
-        dim = get_min_max([node.pos for node in self.nodes])
-        box = dim[1] - dim[0]
+        box = right_lower - left_upper
         return box.len_sq()
+
+    def run(self, node_idx: int, directions: list[int]) -> list[float]:
+        # Evaluates each direction for a given node and calculates the dimension penalty.
+        scores = []
+
+        for direction in directions:
+            # Create a test protein with a new direction for the node
+            test_protein = Protein(self.protein.sequence)
+            test_protein.set_order(self.protein.get_order())
+            test_protein.nodes[node_idx].change_direction(direction)
+
+            # Find bounds and calculate penalty
+            left_upper, right_lower = get_min_max([node.pos for node in self.nodes])
+            penalty = self._calc_size_score(left_upper, right_lower)
+            scores.append(penalty)
+
+        return scores
