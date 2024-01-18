fc06851a835036c2ae671c9a521c1963ad171e9d
wolfg259 <wolf@gautier.nl>
2024-01-18 17:38:01 +0100

    added status bar

--- protein_folding/algorithms/AmadeusFold/data_generator.py
+++ protein_folding/algorithms/AmadeusFold/data_generator.py
@@ -3,22 +3,26 @@ sys.path.append('../../..')
 
 import pandas as pd
 import random
+from tqdm import tqdm
 
 from protein_folding.algorithms.bruteforce import BruteForce
 from protein_folding.protein import Protein
 from sequence_generator import sequence_generator
 
 def data_generator(
-	n_sequences: int = 3,
+	n_datapoints: int = 10000,
 	seq_len_limits: list = [5, 10],
-	max_bruteforce_iterations: int = 10000,
+	max_bruteforce_iterations: int = 100000,
 	csv_loc: str = './data/test_data.csv'
 	) -> None:
 
 	dataset = list()
 
 	# loop over number of desired sequences
-	for _ in range(n_sequences):
+	i = 0
+	pbar = tqdm(total=n_datapoints)
+	while i < n_datapoints:
+
 		# generate a random sequence of random length within bounds
 		n_molecules = random.randint(seq_len_limits[0], seq_len_limits[1])
 		sequence = sequence_generator(n_molecules)
@@ -36,10 +40,15 @@ def data_generator(
 
 			datapoint = [sequence, score, order, coordinates]
 			dataset.append(datapoint)
+			i += 1
+			pbar.update(1)
+	
+	pbar.close()
 	
 	# convert to dataframe and save
 	df = pd.DataFrame(dataset, columns=['sequence', 'score', 'order', 'coordinates'])
 	df.to_csv(csv_loc)
+
 
 if __name__ == "__main__":
 	data_generator()
