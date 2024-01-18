0f3f3e949f50dcc4f79a011943646032991025bc
saif <saif-aknouch@hotmail.com>
2024-01-18 17:47:46 +0100

    moved calc_size_score function from protein and imported get_min_max

--- protein_folding/algorithms/heuristics/minimizedimensions.py
+++ protein_folding/algorithms/heuristics/minimizedimensions.py
@@ -1,10 +1,17 @@
 from . import Heuristic
 from protein_folding.protein import Protein
 from protein_folding.node import Node
-from protein_folding.vec3d import Vec3D  # Assuming Vec3D is a class for 3D vector operations
+from protein_folding.vector.tools import get_min_max  
 
 class MinimizeDimensions(Heuristic):
     def __init__(self, *args):
         super().__init__(*args)
 
-
+    def _calc_size_score(self):
+        """
+        Computes a score based on the physical dimensions of the protein. This
+            value can be used as a penalty for a specific configuration.
+        """
+        dim = get_min_max([node.pos for node in self.nodes])
+        box = dim[1] - dim[0]
+        return box.len_sq()
