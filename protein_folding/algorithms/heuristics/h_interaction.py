eeccbfe8224e13cbfd2d191c7b3b16afc5fadceb
saif <saif-aknouch@hotmail.com>
2024-01-19 11:25:06 +0100

    New H interaction heuristic

---
+++ protein_folding/algorithms/heuristics/h_interaction.py
@@ -0,0 +1,23 @@
+
+from . import Heuristic
+from protein_folding.vector import Vec3D
+
+class HydrophobicInteraction(Heuristic):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.hydrophobic_atoms = [node for node in self.protein.nodes if node.is_hydrophobic()]
+
+    def _calculate_potential(self, atom):
+        # Compute the potential for a single hydrophobic atom
+        potential = 0
+        for other_atom in self.hydrophobic_atoms:
+            if other_atom != atom:
+                distance = Vec3D.distance(atom.position, other_atom.position)
+                potential -= 1 / distance if distance != 0 else float('inf')
+        return potential
+
+    def run(self):
+        total_potential = 0
+        for atom in self.hydrophobic_atoms:
+            total_potential += self._calculate_potential(atom)
+        return total_potential
