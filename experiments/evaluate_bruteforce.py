8509dcc2448c15b0fbea6e6eff7057334d11b7e5
Jarecs <Jarecschouten@gmail.com>
2024-01-19 11:07:55 +0100

    Restructured bruteforce

--- experiments/evaluate_bruteforce.py
+++ experiments/evaluate_bruteforce.py
@@ -4,32 +4,37 @@ import pstats
 from experiments_helper import create_bruteforce_folders
 from protein_folding.protein import Protein
 from protein_folding.algorithms import BruteForce
-
+import time
 
 def run():
     # Create necessary folders
     create_bruteforce_folders()
 
-    seq = "CCCCCCC"
+    seq = "HHPHHHPHPHHHPH"
     max_iterations = 5000000
     dim = 2
     protein = Protein(seq)
-    algorithm = BruteForce(protein, dimensions=dim, max_iterations=max_iterations)
+    algorithm = BruteForce(protein, dimensions=dim, max_iterations=max_iterations, verbose=True)
 
     results = algorithm.run()
     print(results)
     print(f"Lowest score: {min(results.values())}")
 
-    for i, order in enumerate(results.keys()):
-        protein = Protein(seq)
-        protein.set_order(order)
-        score = results[order]
-        protein.plot(filename=f'./output/bf_output/{algorithm.get_name()}_#{i + 1}_score{score}.png')
+    # for i, order in enumerate(results.keys()):
+    #     protein = Protein(seq)
+    #     protein.set_order(order)
+    #     score = results[order]
+    #     print(f"{i}: {score}")
+        # protein.plot(filename=f'./output/bf_output/{algorithm.get_name()}_#{i + 1}_score{score}.png')
 
 
 if __name__ == '__main__':
-    profiler = cProfile.Profile()
-    profiler.runcall(run)
-    stats = pstats.Stats(profiler)
-    stats.strip_dirs().sort_stats('cumulative').print_stats(10)
-    profiler.dump_stats('output/protein.prof')
+    # profiler = cProfile.Profile()
+    # profiler.runcall(run)
+    # stats = pstats.Stats(profiler)
+    # stats.strip_dirs().sort_stats('cumulative').print_stats(10)
+    # profiler.dump_stats('output/protein.prof')
+    start_time = time.perf_counter()
+    run()
+    print(time.perf_counter() - start_time)
+
