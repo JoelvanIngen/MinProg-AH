43e92442b5bd34f1ca84fb7000f6a2b6261e3fb9
wolfg259 <wolf@gautier.nl>
2024-01-21 21:08:27 +0100

    changed normalisation procedure

--- protein_folding/algorithms/AmadeusFold/dataset.py
+++ protein_folding/algorithms/AmadeusFold/dataset.py
@@ -1,11 +1,19 @@
 from torch.utils.data import Dataset
 from torch.nn.functional import normalize
 from torchvision.transforms import ToTensor
+import torch
 import numpy as np
 import pandas as pd
 import ast
 
+from utils import index_sequence
+
 class FoldDataset(Dataset):
+	"""
+	Dataset class to accommodate protein folding data. To normalise, all
+	coordinates are divided by the maximum distance from 0 found in any
+	direction in the provided .csv.
+	"""
 	def __init__(
 			self,
 			csv_location='./data/test_data.csv',
@@ -13,12 +21,24 @@ class FoldDataset(Dataset):
 			transform = ToTensor(),
 			normalize = True
 			) -> None:
-		self.dataframe = pd.read_csv(csv_location)
 		self.transform = transform
 		self.normalize = normalize
+		
+		# read in .csv file
+		self.dataframe = pd.read_csv(csv_location)
 		if shuffle:
 			self.dataframe = self.dataframe.sample(frac = 1)
-	
+
+		# find maximum coordinate value in any direction
+		self.max_coordinate_value = 0
+		for idx in range(len(self.dataframe)):
+			datapoint = self.dataframe.iloc[idx]
+			coordinates_list = ast.literal_eval(datapoint['coordinates'])
+			for coordinates in coordinates_list:
+				for coordinate in coordinates:
+					if abs(coordinate) > self.max_coordinate_value:
+						self.max_coordinate_value = abs(coordinate)
+
 	def __len__(self) -> int:
 		return len(self.dataframe)
 	
@@ -29,8 +49,9 @@ class FoldDataset(Dataset):
 
 		if self.transform:
 			coordinates = self.transform(coordinates)
-			if self.normalize:
-				coordinates = normalize(coordinates, dim = 1)
+		# divide by maximum found value if normalized
+		if self.normalize:
+			coordinates/= self.max_coordinate_value
 				
 		return sequence, coordinates
 
