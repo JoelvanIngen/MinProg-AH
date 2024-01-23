20fb2adcc9b5fe3dd6953ed80870af9382dd1bcc
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-23 12:28:18 +0100

    Add valid configuration counter for bruteforce.py

--- protein_folding/algorithms/bruteforce.py
+++ protein_folding/algorithms/bruteforce.py
@@ -70,11 +70,14 @@ class BruteForce(Algorithm):
             self.max_configs = 4 ** self.n
         self.score_tracker = ScoreTracker(max_scores=10)
 
+        self.valid_configurations_found = 0
+
     def run(self) -> dict:
         # for i, order in zip(range(self.configs), self.order_list):
         order = tuple([-2] * (len(self.protein.sequence) - 1))
         for _ in range(self.max_configs):
             if fast_validate_protein(order):
+                self.valid_configurations_found += 1
                 score = fast_compute_bond_score(seq=self.sequence, order=order)
                 if score < self.score_tracker.lowest_top_score:
                     if self.verbose:
