2ccf097ebe66075b240896a59fda36cf1655294a
wolfg259 <wolf@gautier.nl>
2024-01-24 16:17:44 +0100

    implemented custom loss function

--- protein_folding/algorithms/AmadeusFold/custom_loss.py
+++ protein_folding/algorithms/AmadeusFold/custom_loss.py
@@ -2,29 +2,69 @@ import torch
 import torch.nn as nn
 
 from dataset import FoldDataset
-from utils import compute_bond_score_coordinates, fold_validity_quantified_loss
+from utils import compute_bond_score_coordinates, fold_validity_quantified_loss, StraightThroughEstimator
 
 
 class FoldLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
+	def __init__(
+		self,
+		rounding_scale = 1.0,
+		validity_scale = 1.0,
+		bond_qual_scale = 1.0
+	):
+		super().__init__()
+		self.rounding_scale = rounding_scale
+		self.validity_scale = validity_scale
+		self.bond_qual_scale = bond_qual_scale
 
-    def forward(self, predictions, targets):
-		# TODO: 
-		# - add invalid loss
-		# - add loss if valid but less good bond than optimal
-		# - add loss of difference between float and rounded int coordinates (ie 1.25 is a bad output, should be 1.)
+	def forward(
+		self, 
+		predictions, 
+		norm_factor, 
+		sequence,
+		targets, 
+		target_score,
+	):
+		# un-normalise predictions and targets
+		predictions *= norm_factor
+		targets *= norm_factor
 
-        return loss
+		# round output with straight-through estimator (differentiable)
+		rounded_predictions = StraightThroughEstimator.apply(predictions)
+
+		# determine rounding error, scale and add to loss
+		rounding_error = torch.mean((predictions - rounded_predictions)**2)
+
+		# determine validity, scale and add to loss
+		validity = fold_validity_quantified_loss(rounded_predictions)
+
+		# if presented ordering is valid compute score difference
+		if validity == 0:
+			prediction_score = compute_bond_score_coordinates(sequence, predictions)
+			score_diff = torch.abs(prediction_score - target_score)
+			# unless similar score is achieved, compare to target coordinates
+			if score_diff != 0:
+				score_diff += torch.abs(torch.sum(rounded_predictions - targets))
+		# to avoid creating minima going from invalid orders to valid orders
+		# with bad scores, add the target score as minimum score loss
+		else:
+			score_diff = -target_score
+
+		# calculate total loss
+		loss = (
+			rounding_error * self.rounding_scale +
+			validity * self.validity_scale +
+			score_diff * self.bond_qual_scale
+		)
+			
+		return loss
 
 
 def main():
 	# WORK IN PROGRESS
 	dataset = FoldDataset()
 	sequence = dataset[5][0]
-	coordinates = dataset[5][1] * dataset[5][3]
-
-	print(compute_bond_score_coordinates(sequence, coordinates), dataset[5][2])
+	coordinates = dataset[5][1]
 
 	sample_output = torch.tensor([[[ 0.0589, -0.1306, -0.0682],
 								[ 0.0587, -0.0938, -0.0634],
@@ -32,8 +72,15 @@ def main():
 								[ 0.0680, -0.1537, -0.0676],
 								[ 0.0666, -0.1641, -0.0630]]])
 
-	print(fold_validity_quantified_loss(torch.round(sample_output)))
-	print(fold_validity_quantified_loss(coordinates))
+	loss = FoldLoss()
+	l = loss.forward(
+		sample_output, 
+		dataset.norm_val,
+		dataset[5][0],
+		dataset[5][1],
+		dataset[5][2]
+	)
+	print(f"loss: {l}")
 	
 if __name__ == "__main__":
 	main()
