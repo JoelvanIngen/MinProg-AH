c246439538b354e80b909eedb64716d3a38ae188
Joël van Ingen <j.c.ingen@gmail.com>
2024-01-24 20:18:18 +0100

    Add score history plotting to evaluate_depth_first

--- experiments/evaluate_depth_first.py
+++ experiments/evaluate_depth_first.py
@@ -1,4 +1,4 @@
-from experiments_helper import create_experiment_folders, generate_random_sequence
+from experiments_helper import create_experiment_folders, generate_random_sequence, generate_realistic_sequence
 from protein_folding.protein import Protein
 from protein_folding.algorithms import DepthFirst
 from random import shuffle
@@ -7,15 +7,16 @@ import cProfile
 
 def main():
     create_experiment_folders()
-    sequence = generate_random_sequence(12)
-    score = 0
+    sequence = generate_realistic_sequence(18)
     dim = 2
     protein = Protein(sequence)
 
-    algorithm = DepthFirst(protein, dimensions=dim, max_iterations=20000, debug=True)
+    algorithm = DepthFirst(protein, dimensions=dim, max_iterations=20000, debug=True, keep_score_history=True)
     score = algorithm.run()
     print(f"Sequence: {sequence}")
     print(f"Score: {score}")
+
+    algorithm.plot_score_progress()
 
     if dim == 2:
         protein.plot(f'./output/evaluate_{algorithm.get_name()}_len{len(sequence)}_dim{dim}.png')
