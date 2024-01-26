d53ebeb00e387b0e323f2d71479dba37d3deee03
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2024-01-26 11:47:05 +0100

    Change realistic sequence generator for more assignment-like sequences

--- experiments/experiments_helper.py
+++ experiments/experiments_helper.py
@@ -22,7 +22,7 @@ def generate_random_sequence(length):
 
 
 def generate_realistic_sequence(length):
-    return ''.join(random.choices(['H', 'P', 'C'], [0.3, 0.5, 0.2], k=length))
+    return ''.join(random.choices(['H', 'P', 'C'], [0.5, 0.2, 0.3], k=length))
 
 
 def get_available_heuristics():
