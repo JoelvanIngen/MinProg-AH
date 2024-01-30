6be9f3e33476f3c0dc7de7776ffb11555a1da849
Jarecs <Jarecschouten@gmail.com>
2024-01-30 10:49:58 +0100

    Added all experiment strings to greedy and spiral

--- experiments/evaluate_spiral.py
+++ experiments/evaluate_spiral.py
@@ -6,21 +6,23 @@ from random import shuffle
 
 def main():
     create_experiment_folders()
-    # sequence = 'H' * 10
-    # sequence += 'C' * 50
-    # sequence += 'P' * 10
-    # l = list(sequence)
-    # shuffle(l)
-    # sequence = ''.join(l)
-    sequence = "CPPCCPHHCHHPPCHHPC"
+    sequences = ["HHPHHHPHPHHHPH",
+                 "HPHPPHHPHPPHPHHPPHPH",
+                 "PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP",
+                 "HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH",
+                 "PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP",
+                 "CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC",
+                 "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH",
+                 "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"]
 
-    protein = Protein(sequence)
+    for sequence in sequences:
+        protein = Protein(sequence)
 
-    algorithm = Spiral(protein, dimensions=2, debug=True)
-    score = algorithm.run()
-    print(f"Score: {score}")
+        algorithm = Spiral(protein, dimensions=3, debug=True)
+        score = algorithm.run()
+        print(f"Sequence: {sequence} Score: {score}")
 
-    protein.plot(f'./output/{algorithm.get_name()}_len{len(sequence)}.png')
+        # protein.plot(f'./output/{algorithm.get_name()}_len{len(sequence)}.png')
 
 
 if __name__ == '__main__':
